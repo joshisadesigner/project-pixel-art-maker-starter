48231631f87c94f9ef470bc53791cae99a8ecbc1
Josh <joshisadesigner@gmail.com>
2017-09-13 14:54:36 -0600

    Add functions to add grid and change cell color

--- designs.js
+++ designs.js
@@ -2,9 +2,33 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+var colorPicker = $('#colorPicker'),
+    canvas = $('#pixel_canvas');
 
 function makeGrid() {
 
 // Your code goes here!
 
+  var gridHeight = $('#input_height'),
+      gridWidth = $('#input_width');
+
+  canvas.children().remove();
+
+  for ( var i = 0; i < gridHeight.val(); i++ ){
+    canvas.append( '<tr></tr>' );
+  }
+
+  for ( var i = 0; i < gridWidth.val(); i++ ){
+    canvas.find( 'tr' ).append( '<td class="cell"></td>' );
+  }
 }
+
+$( '#sizePicker' ).submit( function( event ) {
+  makeGrid()
+  event.preventDefault();
+} );
+
+canvas.on( 'click', 'td', function() {
+  console.log( 'click' );
+  $( this ).css( 'background-color', colorPicker.val() );
+});
